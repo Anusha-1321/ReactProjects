a90beeb36746bf766a726d2e80368adccb1d4f2e
Anusha-1321 <anusha.jotwani@etatvasoft.com>
2024-06-11 09:10:25 +0530

    Learned About Hooks and Context API in React

--- hook-sample-project/src/App.js
+++ hook-sample-project/src/App.js
@@ -1,30 +1,62 @@
-import React, { useState } from 'react';
+// , { useState, useEffect }
+import React, { useContext } from "react";
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
+import AuthContext from "./store/auth-context";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  // const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  const loginHandler = (email, password) => {
-    // We should of course check email and password
-    // But it's just a dummy/ demo anyways
-    setIsLoggedIn(true);
-  };
+  // //useEffect'll execute for the 1st time when the app component is called ,
+  // //then it runs only if there r any dependency changes
+  // //here we don't have any dependencies so it'll run only once, during the start of application
+  // //Thus it'll solve our reloadd problem[loggedIn state disapearing when page reloads]
+  // useEffect(() => {
+  //   const storedUserLoggedInInformation = localStorage.getItem("isLoggedIn");
 
-  const logoutHandler = () => {
-    setIsLoggedIn(false);
-  };
+  //   if (storedUserLoggedInInformation == "1") {
+  //     setIsLoggedIn(true);
+  //   }
+  // }, []);
+
+  // const loginHandler = (email, password) => {
+  //   // We should of course check email and password
+  //   // But it's just a dummy/ demo anyways
+  //   localStorage.setItem("isLoggedIn", "1"); //localStorage => global var
+  //   //using localStorage we can store data into the browser
+  //   setIsLoggedIn(true);
+  // };
+
+  // const logoutHandler = () => {
+  //   setIsLoggedIn(false);
+  // };
+
+  const ctx = useContext(AuthContext);
 
   return (
-    <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+
+      // <AuthContext.Provider value={{
+      //   isLoggedIn: isLoggedIn,
+      //   onLogout: logoutHandler
+      // }}>
+      //   {/* isAuthenticated={isLoggedIn} */}
+      //   <MainHeader  onLogout={logoutHandler} />
+      //   <main>
+      //     {!isLoggedIn && <Login onLogin={loginHandler} />}
+      //     {isLoggedIn && <Home onLogout={logoutHandler} />}
+      //   </main>
+      // </AuthContext.Provider>
+
+      <React.Fragment>
+      <MainHeader />
       <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
+        {!ctx.isLoggedIn && <Login />}
+        {ctx.isLoggedIn && <Home />}
       </main>
     </React.Fragment>
+
   );
 }
 
--- hook-sample-project/src/components/Login/Login.js
+++ hook-sample-project/src/components/Login/Login.js
@@ -1,43 +1,120 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useReducer, useContext } from "react";
 
-import Card from '../UI/Card/Card';
-import classes from './Login.module.css';
-import Button from '../UI/Button/Button';
+import Card from "../UI/Card/Card";
+import classes from "./Login.module.css";
+import Button from "../UI/Button/Button";
+import AuthContext from '../../store/auth-context';
+
+//we have create dthe reducer fn outside of the component fn
+//we did so bcoz inside of the reducer fn we won't need data i.e. generated inside the component fn
+//all the data that'll be required and used inside of the reducer fn'll be passed into this fn when it's executed by react automatically
+const emailReducer = (state, action) => {
+  if (action.type === "USER_INPUT") {
+    return { value: action.val, isValid: action.val.includes("@") };
+  }
+  if (action.type === "INPUT_BLUR") {
+    return { value: state.value, isValid: state.value.includes("@") };
+  }
+  return { value: "", isValid: false };
+};
+
+const passwordReducer = (state, action) => {
+  if (action.type === "USER_INPUT") {
+    return { value: action.val, isValid: action.val.trim().length > 6 };
+  }
+  if (action.type === "INPUT_BLUR") {
+    return { value: state.value, isValid: state.value.trim().length > 6 };
+  }
+  return { value: "", isValid: false };
+};
 
 const Login = (props) => {
-  const [enteredEmail, setEnteredEmail] = useState('');
-  const [emailIsValid, setEmailIsValid] = useState();
-  const [enteredPassword, setEnteredPassword] = useState('');
-  const [passwordIsValid, setPasswordIsValid] = useState();
+  // const [enteredEmail, setEnteredEmail] = useState('');
+  // const [emailIsValid, setEmailIsValid] = useState();
+  // const [enteredPassword, setEnteredPassword] = useState('');
+  // const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
-  const emailChangeHandler = (event) => {
-    setEnteredEmail(event.target.value);
+  //we'll create useReduvcer for email and pwd separately
+  const [emailState, dispatchEmail] = useReducer(emailReducer, {
+    value: "",
+    isValid: null,
+  });
+  const [passwordState, dispatchPassword] = useReducer(passwordReducer, {
+    value: "",
+    isValid: null,
+  });
 
-    setFormIsValid(
-      event.target.value.includes('@') && enteredPassword.trim().length > 6
-    );
+  const authCtx = useContext(AuthContext);
+
+  //summary - practice
+  useEffect(() => {
+    console.log("EFFECT RUNNING");
+
+    return () => {
+      console.log("EFFECT CLEANUP");
+    };
+  }, [passwordState.value]);
+
+  //obj destructuring - can be used with both reducer and props
+  const {isValid: emailIsValid} = emailState;
+  const {isValid: passwordIsValid} = passwordState;
+
+  useEffect(() => {
+    console.log('Checking form validity!');
+    //Initially we were checking form validity on every key stroke, this is a very expensive jo
+    //we certainly don't wat to call the fn to check if the username and pwd r clear at every key stroke
+    //This method is called Debouncing - we want to debounce the user input - means that we don't want to do anything to the userInput on every keystroke
+    //                                   but once when the user made a pause during typing
+    //so instead we use the setTimeOut fn and call it every 500ms until we execute some fn.
+    //this still doesn't do much
+    //It execcutes the fn for the same amount of time but after the wait interval is ove
+    //The trick is to use the clean up fn along with the setTimeout() fn - i.e. save the timer and clear it before the next key stroke.
+    //so that we only have 1 ongoing timer at a time
+    // so we'll have only the last timer that'll complete after 500ms
+    const identifier = setTimeout(() => {
+      setFormIsValid(
+        emailState.isValid && passwordState.isValid
+      );
+    }, 500);
+
+    //Cleanup fn. => Its runs before every execution of the effect fn [except  the 1st execution]]
+    //               and before the component is removed
+    return () => {
+      console.log("Cleanup");
+      clearTimeout(identifier);
+    };
+  },[ emailIsValid, passwordIsValid]);
+
+  const emailChangeHandler = (event) => {
+    //setEnteredEmail(event.target.value);
+
+    //this action triggers our reducer fn.
+    dispatchEmail({ type: "USER_INPUT", val: event.target.value });
+    //setFormIsValid(event.target.value.includes("@") && passwordState.isValid);
   };
 
   const passwordChangeHandler = (event) => {
-    setEnteredPassword(event.target.value);
+    //setEnteredPassword(event.target.value);
 
-    setFormIsValid(
-      event.target.value.trim().length > 6 && enteredEmail.includes('@')
-    );
+    dispatchPassword({ type: "USER_INPUT", val: event.target.value });
+    //setFormIsValid(emailState.isValid && event.target.value.trim().length > 6);
   };
 
   const validateEmailHandler = () => {
-    setEmailIsValid(enteredEmail.includes('@'));
+    //setEmailIsValid(emailState.isValid);
+    
+    dispatchEmail({ type: "INPUT_BLUR" });
   };
 
   const validatePasswordHandler = () => {
-    setPasswordIsValid(enteredPassword.trim().length > 6);
+    //setPasswordIsValid(enteredPassword.trim().length > 6);
+    dispatchPassword({ type: "INPUT_BLUR" });
   };
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(enteredEmail, enteredPassword);
+    authCtx.onLogin(emailState.value, passwordState.value);
   };
 
   return (
@@ -45,28 +122,28 @@ const Login = (props) => {
       <form onSubmit={submitHandler}>
         <div
           className={`${classes.control} ${
-            emailIsValid === false ? classes.invalid : ''
+            emailState.isValid === false ? classes.invalid : ""
           }`}
         >
           <label htmlFor="email">E-Mail</label>
           <input
             type="email"
             id="email"
-            value={enteredEmail}
+            value={emailState.value}
             onChange={emailChangeHandler}
             onBlur={validateEmailHandler}
           />
         </div>
         <div
           className={`${classes.control} ${
-            passwordIsValid === false ? classes.invalid : ''
+            passwordState.isValid === false ? classes.invalid : ""
           }`}
         >
           <label htmlFor="password">Password</label>
           <input
             type="password"
             id="password"
-            value={enteredPassword}
+            value={passwordState.value}
             onChange={passwordChangeHandler}
             onBlur={validatePasswordHandler}
           />
